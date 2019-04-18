ba375aef1eb5c1959743b47235fb09c048e8a955
Jean-Baptiste Mérand <jbmerand@tutanota.com>
2019-04-18 11:26:24 +0200

    TP7 finished, last version

--- src/main/java/fr/algorithmie/ComparaisonTableau.java
+++ src/main/java/fr/algorithmie/ComparaisonTableau.java
@@ -2,28 +2,29 @@ package fr.algorithmie;
 
 /**
  * @author Jean-Baptiste
- * version simple
- * TODO : optimisable pour gérer les doublons
+ * version 2.0 : gestion des doublons
  */
 public class ComparaisonTableau {
 
 	public static void main(String[] args) {
-		int[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, 17, 2, 3, 0, 14, -4};
-		int[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8};
+		Integer[] array1 = {1, 15, -3, 8, 7, 4, -2, 28, -1, -1, -1, 3, 0, 14, -4};
+		Integer[] array2 = {3, -8, 17, 5, -1, 4, 0, 6, 2, 11, -5, -4, 8};
 		
 		int sharedElements = 0;
 		
-		for (int i : array2) {
-			for (int j : array1) {
-				if (i == j) {
+		for (Integer i : array2) {
+			for (Integer j : array1) {
+				if (i.equals(j)) {
 					sharedElements = sharedElements + 1;
+					j = null;
+					break; // faudrait break la boucle sur array2
 				}
 			}
 		}
 		
 		// displays to verify
 		System.out.println("Nombre d'éléments en commun : \n" + sharedElements);
-
+		
 	}
 
 }
--- src/main/java/fr/algorithmie/Interactif21Batons.java
+++ src/main/java/fr/algorithmie/Interactif21Batons.java
@@ -1,17 +1,70 @@
 package fr.algorithmie;
 
+import java.util.Scanner;
+import java.util.Random;
+
 /**
  * @author Jean-Baptiste
- * TODO : à finir
+ * 
  */
 public class Interactif21Batons {
 
 	public static void main(String[] args) {
+		
+		// VARIABLES
 		int remainingSticks = 21;
+		int playerChoice = 0;
+		int computerChoice = 0;
+		int turnCounter = 0;
+		boolean isUserWinner = false;
+		boolean isComputerWinner = false;
+		Scanner scanner = new Scanner(System.in);
+		
+		// PROGRAM
 		System.out.println("******** JEU DES BATONS ********");
-		System.out.println("Règles : il y a 21 batons. L'ordinateur est l'autre joueur.\nA chaque tour, un joueur choisit entre 1 et 3 batons.\nSi jamais il prend le dernier baton, il perd.");
+		System.out.println("Règles : il y a 21 batons. L'ordinateur est l'autre joueur.\nA chaque tour, un joueur choisit entre 1 et 3 batons.\nSi jamais il prend le dernier baton, il perd.\n");
 		
+		// 1 iteration = 1 complete turn
+		while (remainingSticks > 0) {
+			turnCounter++; 
+			// user turn
+			do {
+				System.out.println("**********\nTOUR DU JOUEUR N°" + turnCounter + " :\n**********");
+				System.out.println("Combien de batons prenez-vous (1 à 3) ?");
+				playerChoice = scanner.nextInt();
+			} while (playerChoice < 1 || playerChoice > 3);
+			
+			remainingSticks -= playerChoice;
+			if (remainingSticks <= 0) {
+				isComputerWinner = true;
+				break;
+			} else {
+				System.out.println("=> Il reste " + remainingSticks + " batons.\n----------");
+			}
+			
+			// computer turn
+			System.out.println("**********\nTOUR DE L'ORDINATEUR N°" + turnCounter + " :\n**********");
+			Random randNumber = new Random(); // creates random object
+			computerChoice = randNumber.nextInt(3) + 1; // number from 1 to 3
+			System.out.println("L'ordinateur prend " + computerChoice + " baton(s).");
+			remainingSticks -= computerChoice;
+			if (remainingSticks <= 0) {
+				isUserWinner = true;
+				break;
+			} else {
+				System.out.println("=> Il reste " + remainingSticks + " batons.\n----------");
+			}
+	
+		}
 		
+		// displays the defeat or victory message
+		if (isUserWinner == true) {
+			System.out.println("Bravo, vous avez gagné !!");
+		} else if (isComputerWinner == true){
+			System.out.println("GAME OVER :/\nL'ordinateur gagne !");
+		}
+	
+		scanner.close();
 	}
 
 }
